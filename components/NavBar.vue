9015018e78184fad6d2988d82d7f61e9948c81b7
Sebastian Too <sengzhao.95@gmail.com>
2021-03-25 03:03:36 +0800

    package boilerplate

---
+++ components/NavBar.vue
@@ -0,0 +1,70 @@
+<template>
+  <nav v-if="visible" class="navbar">
+    <router-link v-for="(l, idx) in links" :key="idx" :to="l.link" class="navlinks" exact>{{ l.title }}</router-link>
+  </nav>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      disableView: [], // disable for certain routes 
+      links: [
+        {
+          title: 'home',
+          link: '/'
+        },
+        {
+          title: 'route1',
+          link: '/'
+        },
+          {
+            title: 'route2',
+            link: '/'
+          },
+        {
+          title: 'route3',
+          link: '/'
+        }
+      ]
+    }
+  },
+  computed: {
+    visible () {
+      if (this.disableView.includes(this.$route.name)) {
+        return false;
+      } else {
+        return true;
+      }
+    },
+  },
+}
+</script>
+
+<style>
+.navbar {
+  margin-top: 25vh;
+  margin-bottom: 10vh;
+  text-align: center;
+}
+
+/* Enable highlighting when routes are defined */
+/* .navbar a.nuxt-link-exact-active {
+  color: #00c58e;
+  border-bottom: solid .5px;
+} */
+
+.navlinks {
+  margin-left: 1vw;
+  margin-right: 1vw;
+  font-size: .75em;
+  text-decoration: none;
+  color: white;
+  text-transform: uppercase;
+}
+
+.navlinks:hover {
+  color: #00c58e;
+  border-bottom: solid .5px;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template> 
+  <div class="container">
+    <div class="home__title"> {{ title }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: 'my nuxtjs boilerplate',
+    }
+  }
+}
+</script>
+
+<style>
+.home__title {
+  min-height: 25vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: auto; 
+  text-align: center;
+  color: white;
+}
+</style>
